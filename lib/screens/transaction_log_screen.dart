8e4b9888472d6168eaaa2e3c2afa59aa185b677d
Phi Le <ple31@student.gsu.edu>
2025-03-18 23:54:48 -0400

    Update transaction_log_screen UI to accommodate for smaller screens - Input fields now wrap to next line if needed - Form resets on submit - Transaction table expanded to fill container - Added hint to category dropdown button

--- lib/screens/transaction_log_screen.dart
+++ lib/screens/transaction_log_screen.dart
@@ -46,34 +46,44 @@ class _TransactionLogScreenState extends State<TransactionLogScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: const FinanceMangerAppBar(),
-        drawer: const FinanceMangerAppDrawer(),
-        body: SingleChildScrollView(
-            padding: const EdgeInsets.only(top: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width / 3,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(color: Color(0xFF144664)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: const Text('Transaction Log',
-                        style: TextStyle(color: Colors.white)),
-                  ),
+      appBar: const FinanceMangerAppBar(),
+      drawer: const FinanceMangerAppDrawer(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(top: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width / 3,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(color: Color(0xFF144664)),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: const Text(
+                  'Transaction Log',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
                 ),
-                Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TransactionForm(
-                        onSave: _saveTransaction, categories: categories)),
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: TransactionTable(
-                      transactions: transactions, categories: categories),
-                )
-              ],
-            )));
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TransactionForm(
+                onSave: _saveTransaction,
+                categories: categories,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: TransactionTable(
+                transactions: transactions,
+                categories: categories,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
 
@@ -114,6 +124,15 @@ class _TransactionFormState extends State<TransactionForm> {
     super.dispose();
   }
 
+  void resetForm() {
+    costController.clear();
+    descriptionController.clear();
+    setState(() {
+      date = DateTime.now();
+      selectedCategory = null;
+    });
+  }
+
   void _onTextChanged() {
     setState(() {});
   }
@@ -127,50 +146,61 @@ class _TransactionFormState extends State<TransactionForm> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        clipBehavior: Clip.hardEdge,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5), border: Border.all()),
-        child: Padding(
-            padding: EdgeInsets.all(8),
-            child: Column(spacing: 8, children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: costController,
-                      decoration: const InputDecoration(
-                        labelText: 'Cost',
-                        border: OutlineInputBorder(),
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        border: Border.all(),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 8,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: costController,
+                    decoration: const InputDecoration(
+                      labelText: 'Cost',
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.numberWithOptions(
+                      signed: true,
+                      decimal: true,
+                    ),
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.allow(
+                        RegExp(r'^-?[0-9]*(\.[0-9]{0,2})?$'),
                       ),
-                      keyboardType: TextInputType.numberWithOptions(
-                        signed: true,
-                        decimal: true,
-                      ),
-                      inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.allow(
-                            RegExp(r'^-?[0-9]*(\.[0-9]{0,2})?$')),
-                      ],
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: TextField(
+                    maxLength: 100,
+                    controller: descriptionController,
+                    decoration: const InputDecoration(
+                      labelText: 'Description',
+                      border: OutlineInputBorder(),
                     ),
                   ),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: TextField(
-                      maxLength: 100,
-                      controller: descriptionController,
-                      decoration: const InputDecoration(
-                        labelText: 'Description',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(children: [
-                Container(
+                ),
+              ],
+            ),
+            Wrap(alignment: WrapAlignment.start, runSpacing: 8, children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 12),
+                child: Container(
+                    clipBehavior: Clip.hardEdge,
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        border: Border.all(color: Colors.grey)),
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(color: Colors.grey),
+                    ),
+                    constraints: const BoxConstraints(maxWidth: 200),
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Row(children: [
@@ -193,55 +223,67 @@ class _TransactionFormState extends State<TransactionForm> {
                         ),
                       ]),
                     )),
-                SizedBox(width: 12),
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      border: Border.all(color: Colors.grey)),
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 8, vertical: 3),
-                    child: DropdownButton<int>(
-                      value: selectedCategory,
-                      onChanged: (newValue) {
-                        setState(() {
-                          selectedCategory = newValue!;
-                        });
-                      },
-                      items: widget.categories.entries
-                          .map((entry) => DropdownMenuItem<int>(
-                                value: entry.key,
-                                child: Text(entry.value),
-                              ))
-                          .toList(),
-                    ),
+              ),
+              Container(
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  border: Border.all(color: Colors.grey),
+                ),
+                constraints: const BoxConstraints(maxWidth: 200),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 3),
+                  child: DropdownButton<int>(
+                    value: selectedCategory,
+                    hint: const Text('Select Category'),
+                    borderRadius: BorderRadius.circular(5),
+                    onChanged: (newValue) {
+                      setState(() {
+                        selectedCategory = newValue!;
+                      });
+                    },
+                    items: widget.categories.entries
+                        .map((entry) => DropdownMenuItem<int>(
+                              value: entry.key,
+                              child: Text(entry.value),
+                            ))
+                        .toList(),
                   ),
                 ),
-                Expanded(child: Container()),
-                IconButton(
-                    onPressed: _areFieldsValid()
-                        ? () {
-                            final newTransaction = UserTransaction(
-                              date: date,
-                              cost: double.parse(costController.text),
-                              description: descriptionController.text,
-                              categoryId: selectedCategory!,
-                            );
-                            widget.onSave(newTransaction);
-                          }
-                        : null,
-                    style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty.all(
-                          _areFieldsValid()
-                              ? Color(0xFFDAE8FC)
-                              : const Color.fromARGB(255, 204, 204, 204),
-                        ),
-                        shape: WidgetStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
-                        ))),
-                    icon: const Icon(Icons.add)),
-              ])
-            ])));
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: IconButton(
+                  onPressed: _areFieldsValid()
+                      ? () {
+                          final newTransaction = UserTransaction(
+                            date: date,
+                            cost: double.parse(costController.text),
+                            description: descriptionController.text,
+                            categoryId: selectedCategory!,
+                          );
+                          widget.onSave(newTransaction);
+                          resetForm();
+                        }
+                      : null,
+                  style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        _areFieldsValid()
+                            ? Color(0xFFDAE8FC)
+                            : const Color.fromARGB(255, 204, 204, 204),
+                      ),
+                      shape: WidgetStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ))),
+                  icon: const Icon(Icons.add),
+                ),
+              ),
+            ]),
+          ],
+        ),
+      ),
+    );
   }
 }
 
@@ -259,26 +301,26 @@ class TransactionTable extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(5), border: Border.all()),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                border: Border(bottom: BorderSide()),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(5),
-                  topRight: Radius.circular(5),
-                ),
-                color: Theme.of(context).colorScheme.inversePrimary),
-            child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Transactions',
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18))),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+        Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              border: Border(bottom: BorderSide()),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(5),
+                topRight: Radius.circular(5),
+              ),
+              color: Theme.of(context).colorScheme.inversePrimary),
+          child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: const Text('Transactions',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18))),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Container(
+            constraints:
+                BoxConstraints(minWidth: MediaQuery.of(context).size.width),
             child: DataTable(
               dataRowMaxHeight: double.infinity,
               columns: const <DataColumn>[
@@ -291,7 +333,8 @@ class TransactionTable extends StatelessWidget {
                 return DataRow(cells: <DataCell>[
                   // maybe set onLongPress to edit a transaction
                   DataCell(
-                      Text(transaction.date.toIso8601String().split('T')[0])),
+                    Text(transaction.date.toIso8601String().split('T')[0]),
+                  ),
                   DataCell(
                     Text(
                       transaction.cost < 0
@@ -299,15 +342,20 @@ class TransactionTable extends StatelessWidget {
                           : '\$${transaction.cost.toStringAsFixed(2)}',
                     ),
                   ),
-                  DataCell(Text(transaction.description)),
+                  DataCell(
+                    Container(
+                      constraints: const BoxConstraints(maxWidth: 100),
+                      child: Text(transaction.description),
+                    ),
+                  ),
                   DataCell(
                       Text(categories[transaction.categoryId] ?? 'Unknown')),
                 ]);
               }).toList(),
             ),
-          )
-        ],
-      ),
+          ),
+        ),
+      ]),
     );
   }
 }
